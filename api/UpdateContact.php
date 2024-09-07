e1a5649a277371efbace697df9710cc1fe33db0e
Frederick B <89982743+bouloutef04@users.noreply.github.com>
2024-09-07 13:56:23 -0400

    Error coded, removed error displaying.

--- api/UpdateContact.php
+++ api/UpdateContact.php
@@ -1,19 +1,24 @@
 <?php
 	$inData = getRequestInfo();
 
+	if (!isset($inData['firstName']) || !isset($inData['lastName']) || !isset($inData['contactID']) || !isset($inData['phoneNumber']) || !isset($inData['email'])) {
+		returnWithError("Invalid input", 400);
+		return;
+	}
+
 	$Id = $inData["contactID"];
 	$firstName = $inData["firstName"];
 	$lastName = $inData["lastName"];
 	$phoneNum = $inData["phoneNumber"];
 	$email = $inData["email"];
 
-	error_reporting(E_ALL);
-	ini_set('display_errors', 1);
+	// error_reporting(E_ALL);
+	// ini_set('display_errors', 1);
 
 	$conn = new mysqli("localhost", "User", "COP4331OMg", "COP4331"); 
 	if ($conn->connect_error)//Check if it can connect
 	{
-		returnWithError( $conn->connect_error );
+		returnWithError( $conn->connect_error, 500);
 	} 
 	else
 	{
@@ -24,11 +29,13 @@
 		{
 			$stmt->close();
 			$conn->close();
-			returnWithError("");
+			returnWithError("", 200);
 		}
 		else
 		{
-			returnWithError("Statement could not execute.");
+			returnWithError("Could not create a contact right now.", 500);
+			$stmt->close();
+			$conn->close();
 		}
 	}
 
@@ -43,10 +50,10 @@
 		echo $obj;
 	}
 	
-	function returnWithError( $err )
-	{
-		$retValue = '{"error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
+	function returnWithError($err, $code) {
+		$retValue = '{"id":-1,"error":"' . $err . '"}';
+		http_response_code($code);
+		sendResultInfoAsJson($retValue);
 	}
 	
 ?>
