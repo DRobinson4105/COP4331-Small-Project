ce90f1bdd7c379d1c693a5468c2325f9fa84f8ac
Frederick B <89982743+bouloutef04@users.noreply.github.com>
2024-09-07 13:46:44 -0400

    Removed error displaying statements. Add error catching and set parameters.

--- api/DeleteContact.php
+++ api/DeleteContact.php
@@ -1,24 +1,39 @@
 <?php
 	$inData = getRequestInfo();
+
+	if (!isset($inData['id'])) {
+		returnWithError("Invalid input", 400);
+		return;
+	}
+
 	$id = $inData["id"];
 
-	error_reporting(E_ALL);
-	ini_set('display_errors', 1);
+	// error_reporting(E_ALL);
+	// ini_set('display_errors', 1);
+
+		if (!isset($inData['login']) || !isset($inData['password'])) {
+		returnWithError("Invalid input", 400);
+		return;
+	}
 
 	$conn = new mysqli("localhost", "User", "COP4331OMg", "COP4331"); 
 	if ($conn->connect_error) 
 	{
-		returnWithError( $conn->connect_error );
+		returnWithError( $conn->connect_error, 500);
 	} 
 	else
 	{
 		//Parameters maybe different as we need a name, phone number, email
 		$stmt = $conn->prepare("DELETE FROM Contacts WHERE ID =?");
 		$stmt->bind_param("s", $id);
-		$stmt->execute();
+		if($stmt->execute()){
+				returnWithError("", 200);
+		}
+		else{
+			returnWithError( "Could not delete contact. Ensure contanct exists.", 500);
+		}
 		$stmt->close();
 		$conn->close();
-		returnWithError("");
 	}
 
 	function getRequestInfo()
@@ -32,10 +47,10 @@
 		echo $obj;
 	}
 	
-	function returnWithError( $err )
-	{
-		$retValue = '{"error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
+	function returnWithError($err, $code) {
+		$retValue = '{"id":-1,"error":"' . $err . '"}';
+		http_response_code($code);
+		sendResultInfoAsJson($retValue);
 	}
 	
 ?>
