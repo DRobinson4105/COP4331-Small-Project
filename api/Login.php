4ad8f080f0b07b11595b90ef562054e2c0ff849c
Frederick B <89982743+bouloutef04@users.noreply.github.com>
2024-09-07 14:18:33 -0400

    Fixed login with error coding. Will test to ensure it works.

--- api/Login.php
+++ api/Login.php
@@ -1,51 +1,66 @@
 <?php
-    $inData = getRequestInfo();
-	
-    $id = 0;
-    $firstName = "";
-    $lastName = "";
+        $inData = getRequestInfo();
 
-    // database credentials
-    $database_username = "User";
-    $database_password = "COP4331OMg";
-    $database_name = "COP4331";
+        if (!isset($inData['login']) || !isset($inData['password'])) {
+                returnWithError("Invalid input", 400);
+                return;
+        }
 
-	$conn = new mysqli("localhost", $database_username, $database_password, $database_name); 	
-	if ($conn->connect_error) {
-		returnWithError( $conn->connect_error );
-    } else {
-		$stmt = $conn->prepare("SELECT ID FROM Users WHERE Login=? AND Password =?");
-		$stmt->bind_param("ss", $inData["login"], $inData["password"]);
-		$stmt->execute();
-		$result = $stmt->get_result();
+        $id = 0;
+        $firstName = "";
+        $lastName = "";
 
-		if ($row = $result->fetch_assoc()) {
-			returnWithInfo($row['ID']);
-		} else {
-			returnWithError("Username or password is wrong");
-		}
+        // database credentials
+        $database_username = "User";
+        $database_password = "COP4331OMg";
+        $database_name = "COP4331";
 
-		$stmt->close();
-		$conn->close();
-	}
-	
-	function getRequestInfo() {
-		return json_decode(file_get_contents('php://input'), true);
-	}
+        $conn = new mysqli("localhost", $database_username, $database_password, $database_name);
+        if ($conn->connect_error) {
+                returnWithError($conn->connect_error, 500);
+        } else {
+                $stmt = $conn->prepare("SELECT ID FROM Users WHERE Login=? AND Password =?");
+                $stmt->bind_param("ss", $inData["login"], $inData["password"]);
+                if ($stmt->execute()) {
+                        $result = $stmt->get_result();
 
-	function sendResultInfoAsJson($obj) {
-		header('Content-type: application/json');
-		echo $obj;
-	}
-	
-	function returnWithError($err) {
-		$retValue = '{"id":0,"error":"' . $err . '"}';
-		sendResultInfoAsJson($retValue);
-	}
-	
-	function returnWithInfo($id) {
-		$retValue = '{"id":' . $id . ',"error":""}';
-		sendResultInfoAsJson($retValue);
-	}
-	
+                        if ($row = $result->fetch_assoc()) {
+                                returnWithInfo($row['ID']);
+                                returnWithError("", 200);
+                        } else {
+                                returnWithError("Username or password is wrong", 400);
+                        }
+                }
+                else{
+                        returnWithError("Could not login right now.", 500);
+                }
+
+                $stmt->close();
+                $conn->close();
+        }
+
+        function getRequestInfo()
+        {
+                return json_decode(file_get_contents('php://input'), true);
+        }
+
+        function sendResultInfoAsJson($obj)
+        {
+                header('Content-type: application/json');
+                echo $obj;
+        }
+
+        function returnWithError($err, $code)
+        {
+                $retValue = '{"id":-1,"error":"' . $err . '"}';
+                http_response_code($code);
+                sendResultInfoAsJson($retValue);
+        }
+
+        function returnWithInfo($id)
+        {
+                $retValue = '{"id":' . $id . ',"error":""}';
+                sendResultInfoAsJson($retValue);
+        }
+
 ?>
