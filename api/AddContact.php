935c10107f6b42939750f1baa0ba12e241471389
Frederick B <89982743+bouloutef04@users.noreply.github.com>
2024-09-11 19:06:37 -0400

    Email Validation

--- api/AddContact.php
+++ api/AddContact.php
@@ -13,38 +13,45 @@
 	$userId = $inData["userId"];
 	$phoneNum = $inData["phoneNumber"];
 	$email = $inData["email"];
-    	$fullName = $fName.' '.$lName;
-
+    	if(filter_var($email, FILTER_VALIDATE_EMAIL))
+	{
+	        $fullName = $fName.' '.$lName;
 	
-	$database_username = "User";
-	$database_password = "COP4331OMg";
-	$database_name = "COP4331";
-
-    	$conn = new mysqli("localhost", $database_username, $database_password, $database_name);
-	if ($conn->connect_error) 
-	{
-		returnWithError( $conn->connect_error, 500);
-	} 
-	else
-	{
-		//Parameters maybe different as we need a name, phone number, email
-		$stmt = $conn->prepare("INSERT into Contacts (UserId,FullName,FirstName,LastName,Phone,Email) VALUES(?,?,?,?,?,?)");
-		$stmt->bind_param("ssssss", $userId, $fullName, $fName, $lName, $phoneNum, $email);
-		if($stmt->execute()){
-			$stmt->close();
-			
-			$Id = $conn->insert_id;
-			$conn->close();
-			sendResultInfoAsJson($Id);
-			returnWithError("", 200);
-		}
-		else
-		{
-			returnWithError("Contact could not be added at this time", 500);
-			$conn->close();
-		}
-		//echo $fName," ", $userId," ", $lName,"", $email," ";
-	}
+	        
+	        $database_username = "User";
+	        $database_password = "COP4331OMg";
+	        $database_name = "COP4331";
+	
+	            $conn = new mysqli("localhost", $database_username, $database_password, $database_name);
+	        if ($conn->connect_error) 
+	        {
+	            returnWithError( $conn->connect_error, 500);
+	        } 
+	        else
+	        {
+	            //Parameters maybe different as we need a name, phone number, email
+	            $stmt = $conn->prepare("INSERT into Contacts (UserId,FullName,FirstName,LastName,Phone,Email) VALUES(?,?,?,?,?,?)");
+	            $stmt->bind_param("ssssss", $userId, $fullName, $fName, $lName, $phoneNum, $email);
+	            if($stmt->execute()){
+	                $stmt->close();
+	                
+	                $Id = $conn->insert_id;
+	                $conn->close();
+	                sendResultInfoAsJson($Id);
+	                returnWithError("", 200);
+	            }
+	            else
+	            {
+	                returnWithError("Contact could not be added at this time", 500);
+	                $conn->close();
+	            }
+	            //echo $fName," ", $userId," ", $lName,"", $email," ";
+	        }
+	    }
+	    else
+	    {
+	        returnWithError("Email is not valid.", 400);
+	    }
 
 	function getRequestInfo()
 	{
