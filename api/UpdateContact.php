7f2de17e58ac37979786f81b0683543b560909b9
Frederick B <89982743+bouloutef04@users.noreply.github.com>
2024-09-11 16:32:21 -0400

    Fixed firstName

--- api/UpdateContact.php
+++ api/UpdateContact.php
@@ -11,7 +11,7 @@
 	$lastName = $inData["lastName"];
 	$phoneNum = $inData["phoneNumber"];
 	$email = $inData["email"];
-    $fullName = $firstfName.' '.$lastName;
+    	$fullName = $firstName.' '.$lastName;
 
 	// error_reporting(E_ALL);
 	// ini_set('display_errors', 1);
